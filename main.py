50c96ca76f0f078363365359a99e0c489e6c9bea
greenSPL27 <greenspl27@gmail.com>
2026-02-24 14:48:31 -0600

    Have a controlable player triangle. Main has been updated to use pygame groups

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from logger import log_state
+
 
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    while True:
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        
+        updatable.update(dt)
         
         screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
-
-        clock.tick(60) / 1000
-        
-
-
-    # print("Starting Asteroids with pygame version: 2.6.1")
-    # print (f"Screen width: {SCREEN_WIDTH}")
-    # print (f"Screen height: {SCREEN_HEIGHT}")
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
